7c5db13ecc57f125d8b62a8e880495cd3560c31b
ruizguille <ruiz.ruiz.guillermo@gmail.com>
2019-11-27 19:38:39 +0100

    Update code

--- parcelas.js
+++ parcelas.js
@@ -3,11 +3,11 @@
 /****************************************************/
 
 // Añadir la clave de acceso para conectarte a la API de Mapbox
-mapboxgl.accessToken = 'pk.eyJ1IjoiY2FydG9tZXRyaWNzIiwiYSI6ImNqOGJ2ZXIzazAxd3kyd3AyMDVrOGpzNWkifQ.KwvwFfoDOeLnjR1gEHO8tg';
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2FydG9tZXRyaWNzbGFiIiwiYSI6ImNrM2JnanIzMjBsZHQzbW1yM2h2OWxieHMifQ.MCULumHFKyTPrBNEETSBww';
 
 // Definición de variables
 var años = ['< 1900', '1900-10', '1910-20', '1920-30', '1930-40', '1940-50', '1950-60','1960-70', '1970-80','1980-90', '1990-2000', '> 2000' ];
-var colores = ['#000000', '#350866', '#58258e', '#8d23a3', '#e01da9', '#e20f95', '#f96945', '#d86a1c', '#f9b445', '#ffda60', '#fff375', '#fffd96'];
+var colores = ['#000000', '#350866', '#57258e', '#8f23a4', '#e21daa', '#e10e94', '#f96743', '#d96b1c', '#f9b343', '#ffda61', '#fff475', '#fffd94'];
 
 /****************************************************/
 /*********************** MAIN ***********************/
@@ -16,8 +16,8 @@ var colores = ['#000000', '#350866', '#58258e', '#8d23a3', '#e01da9', '#e20f95',
 // Crear el mapa
 var mapa = new mapboxgl.Map({
   container: 'mapa', // el elemento HTML con id='mapa' es el contenedor del mapa
-  style: 'mapbox://styles/cartometrics/cj9iau8enap8k2rmocf4a4m3r', // estilo del mapa base inicial
-  center: [-4.428290, 36.720402], // coordenadas [lng, lat] del centro inicial
+  style: 'mapbox://styles/cartometricslab/ck3hk5ggs13n91dpg436umwul', // estilo del mapa base inicial
+  center: [-5.952, 37.388], // coordenadas [lng, lat] del centro inicial
 	zoom: 12, // nivel de zoom inicial
 });
 
@@ -49,25 +49,40 @@ for (i = 0; i < años.length; i++) {
   leyenda.appendChild(elemento);
 }
 
-// Añadir Popups una vez cargue el mapa con la capa de edificios
 mapa.on('load', function () {
-  mapa.on('click', 'buildings-do4vkm', function (e) {
-  	var coordinates = e.lngLat;
-		var año = e.features[0].properties.año;
-		var popupContent = '<h1>PARCELA</h1><div><strong>Año edificio: </strong>'+ año +'</div>'
-		 
-		new mapboxgl.Popup()
-			.setLngLat(coordinates)
-			.setHTML(popupContent)
-			.addTo(mapa);
-	});
+	// Añadir Popups una vez cargue el mapa con la capa de edificios
+	mapa.on('click', 'sevi1-0uh0df', añadirPopup);
+	mapa.on('click', 'sevi2-7em0mi', añadirPopup);
 
 	// Cambiar el cursor del ratón al pasar por encima / salir de una parcela
-	mapa.on('mouseenter', 'buildings-do4vkm', function () {
+	mapa.on('mouseenter', 'sevi1-0uh0df', function () {
 		mapa.getCanvas().style.cursor = 'pointer';
 	});
 
-	mapa.on('mouseleave', 'buildings-do4vkm', function () {
+	mapa.on('mouseleave', 'sevi1-0uh0df', function () {
+		mapa.getCanvas().style.cursor = '';
+	});
+
+	mapa.on('mouseenter', 'sevi2-7em0mi', function () {
+		mapa.getCanvas().style.cursor = 'pointer';
+	});
+
+	mapa.on('mouseleave', 'sevi2-7em0mi', function () {
 		mapa.getCanvas().style.cursor = '';
 	});
 });
+
+
+/****************************************************/
+/******************** FUNCIONES *********************/
+/****************************************************/
+function añadirPopup(e) {
+	var coordinates = e.lngLat;
+	var año = e.features[0].properties.d;
+	var popupContent = '<h1>PARCELA</h1><div><strong>Año edificio: </strong>'+ año +'</div>'
+	 
+	new mapboxgl.Popup()
+		.setLngLat(coordinates)
+		.setHTML(popupContent)
+		.addTo(mapa);
+}
